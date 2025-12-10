45235c3c17849933767b6042be872b488a26934c
Pm Pranta <heyiampm@gmail.com>
2025-12-10 19:45:43 +0600

    add new file

---
+++ Data Type Python.py
@@ -0,0 +1,34 @@
+#Number type data(Numeric type) == int,float,complex.
+#int data type.
+age = 34
+print(type(age))
+
+#string type data
+map = "india"
+print(type(map))
+
+#float type data
+roll = 10.25
+print(type(roll))
+
+#Complex type data
+code = 1221j
+print(type(code))
+
+#Bool type data.// class (bool type data).
+bool = True
+print(bool)
+print(type(bool))
+
+#None type data//class nonetype.
+a = None
+print(a)
+print(type(a))
+
+#Tuple type data//(tup)
+tup = (2,3,45,5)
+print(type(tup))
+
+#Sequency data type,[],(),{}.//class list.
+li = ["mm","t6t","pm","ap"]
+print(type(li))
