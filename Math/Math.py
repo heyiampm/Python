b8623d01520c9ee951717be0091a38d5f629ed91
Pm Pranta <heyiampm@gmail.com>
2025-12-22 00:09:20 +0600

    Python File

---
+++ Math/Math.py
@@ -0,0 +1,18 @@
+# Built-in Math Functions
+
+a = min(10,20,30,40,50)
+b = max(20,30,40,100,200)
+
+print(a)
+print(b)
+
+# abs() function returns the absolute (positive) value of the specified number:
+
+c = -23.4
+print(abs(c))
+
+# The pow(x, y) function returns the value of x to the power of y (xy)
+
+a = pow (4,4)  #this is power function .
+print(a)
+
