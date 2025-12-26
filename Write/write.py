4155753eb5d8660470dac8c77ea5c74ef394ebe7
Pm Pranta <heyiampm@gmail.com>
2025-12-26 12:05:17 +0600

    Python File

---
+++ Write/write.py
@@ -0,0 +1,14 @@
+# write file ,method
+
+# wr =  open("pranta.html", "w")
+# wr.write("<h1> Hey Pranta ,How are you.</h>")
+
+write = open("Mohajan.html", "a")   #append method
+
+write = open("Mohajan.html", "r") 
+
+
+# write.write("<p> hello java programmer </p>\n")
+# write.write("<p> Pranta Mohajan </p>\n")
+# write.write("Yes,iam single\n")
+print(write.read())
